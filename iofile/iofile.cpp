24f22454bfe89c927b8ed1e81c8f972784dbfec3
MusthafaLuthfi76 <musthafa.luthfi76@gmail.com>
2023-06-19 14:43:23 +0700

    Yang di includkan

--- iofile/iofile.cpp
+++ iofile/iofile.cpp
@@ -1,7 +1,7 @@
-// iofile.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
 
 int main()
 {
