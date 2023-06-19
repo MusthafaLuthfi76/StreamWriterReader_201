f6704443bfe46786d91c7fa70bdaa3d77fc17f3c
MusthafaLuthfi76 <musthafa.luthfi76@gmail.com>
2023-06-19 14:52:43 +0700

    Edit yang lain

--- iofile/iofile.cpp
+++ iofile/iofile.cpp
@@ -7,11 +7,27 @@ int main()
 {
     string baris;
 
+    //memasukkan file spesifik
+    //string namafile
+    //cout << "Masukkan namafile";
+    //cin >> namafile
+
     //membuka file dalam mode menulis
     ofstream outfile;
     //menunjuk ke sebuah nama file
     outfile.open("contohfile.txt");
+    //outfile.open(namafile+".txt",ios::out)
+    //outfile.open("D:\Pemrograman Dasar\contoh.txt")
 
     cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+    //unlimited loop untuk menulis
+    while (true) {
+        cout << '- ';
+        //mendapatkan setiap karakter dalam satu baris
+        getline(cin, baris);
+        //loop akan berhenti jika anda memasukkan karakter  q
+        if (baris == "q") break;
+    }
 }
 
