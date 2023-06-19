5e1785844d4ec6f683cc604abbf174a250584ec4
MusthafaLuthfi76 <musthafa.luthfi76@gmail.com>
2023-06-19 15:33:08 +0700

    Manual exception

---
+++ ManualExeption/ManualExeption.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    try {
+        cout << "Selamat Belajar di Prodi TI UMY" << endl;
+        throw 0.5;                                              //Melemparkan sebuah integer maka
+        cout << "Pernyataan tidak akan dieksekusi" << endl;
+    }
+    catch (int a) {
+        //Blok ini akan dieksekusi
+        cout << "Pengecualian akan dieksekusi" << endl;
+    }
+    catch (...) {
+        /*Jika selain integer maka block ini akan dieksekusi*/
+        cout << "default Pengecualian dieksekusi" << endl;
+    }
+
+    return 0;
+}
+
